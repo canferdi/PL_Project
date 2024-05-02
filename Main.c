e58fbd07362bd2dd5601e85dda69ff4efc6bc4f2
Ferdi Can <fcan5414@gmail.com>
2024-05-02 19:05:25 +0300

    Some Method Edited

--- Main.c
+++ Main.c
@@ -3,9 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
-char keyWords[][10] = {"int", "text", "is", "loop", "times", "read", "write", "newLine"};
-
-enum tokenType {
+/*enum tokenType
+{
     IDENTIFIER,
     INTEGER,
     OPERATOR,
@@ -15,68 +14,127 @@ enum tokenType {
     ENDOFLINE,
     COMMA,
     COMMENT
-};
+};*/
 
+char keyWords[][10] = {"int", "text", "is", "loop", "times", "read", "write", "newLine"};
+char operators[] = {'+', '-', '*', '/'};
 
-int identifierControl(const char *str){
-    if(strlen(str) > 10){
+int isInKeywords(char *str)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        if (strcmp(str, keyWords[i]) == 0)
+        {
+            printf("keyword");
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int isInOperators(char *str)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (str[0] == operators[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int identifierControl(char *str)
+{
+    if (strlen(str) > 10)
+    {
         printf("Error: Identifier is too long.\n");
+        return 0;
+    }
+    else if (!isalpha(str[0]))
+    {
+        printf("Error: Identifier must start with a letter.\n");
+        return 0;
     }
     else
     {
-        /* code */
+        printf("Identifier(%s)\n", str);
+        return 0;
     }
-    
-
 }
 
-int integerControl(int num){
-    if(num > 99999999){
-        printf("Error: Integer is too long.\n");
-    }
-    else if (num < 0)
+int integerControl(int num)
+{
+    if (num < 0)
     {
-        printf("Error: Integer is negative.\n");
+        integerControl(num * -1); // Recursive call to make the number positive.
     }
     else
     {
-        printf("IntConts(%d)\n", num);
+        if (num > 99999999)
+        {
+            printf("Error: Integer is too long.\n");
+            return 0;
+        }
+        else if (num < 0)
+        {
+            printf("Error: Integer is negative.\n");
+            return 0;
+        }
+        else
+        {
+            printf("IntConts(%d)\n", num);
+            return 1;
+        }
     }
 }
 
-int operatorControl(){
-   //TODO: Implement operator control.
+int operatorControl(char *str)
+{
+    if (strlen(str) != 1)
+    {
+        return 0;
+    }
+    else if (isInOperators(str))
+    {
+        printf("Operator(%c)\n", str[0]);
+        return 1;
+    }
 }
 
-int bracketControl(){
-    //TODO: Implement bracket control.
+int bracketControl(char *str)
+{
+    // TODO: Implement bracket control.
 }
 
-int stringControl(){
-    //TODO: Implement string control.
+int stringControl(char *str)
+{
+    // TODO: Implement string control.
 }
 
-int keywordControl(){
-    //TODO: Implement keyword control.
+int keywordControl(char *str)
+{
+    // TODO: Implement keyword control.
 }
 
-int endOfLineControl(){
-    //TODO: Implement end of line control.
+int endOfLineControl(char *str)
+{
+    // TODO: Implement end of line control.
 }
 
-int commaControl(){
-    //TODO: Implement comma control.
+int commaControl(char *str)
+{
+    // TODO: Implement comma control.
 }
 
-int commentControl(){
-    //TODO: Implement comment control.
+int commentControl(char *str)
+{
+    // TODO: Implement comment control.
 }
 
+int main()
+{
+    operatorControl("+");
 
-
-
-int main() {
-    integerControl(123);
-    
     return 0;
 }
